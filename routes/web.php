968b5974f7a16db571bc8752d3445800283a1de1
3omarbadr <omarbadr167@gmail.com>
2024-01-02 20:02:10 +0200

    handle tests

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
-});
+})->middleware(['auth', 'verified']);
 
-
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
